0f7fd026a51c37a64ec565b7c13de9d61f7c85d0
Marroar <mar_person@163.com>
2017-09-08 17:28:08 +0800

    space

--- utils/util.js
+++ utils/util.js
@@ -2,7 +2,7 @@ function formatTime(date) {
   var year = date.getFullYear()
   var month = date.getMonth() + 1
   var day = date.getDate()
-
+ 
   var hour = date.getHours()
   var minute = date.getMinutes()
   var second = date.getSeconds()
